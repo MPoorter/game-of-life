fe9321cad7082bda1925a11018b624ff5b55a4d0
Matt <matt.depoorter@marshmallow.co>
2018-11-17 07:06:38 +0000

    Adding Readme.md and renaming to game-of-life

--- src/main/java/com/mattdepoorter/game_of_life/GameOfLifeApplication.java
+++ src/main/java/com/mattdepoorter/game_of_life/GameOfLifeApplication.java
@@ -1,12 +1,12 @@
-package com.mattdepoorter.tree_of_life;
+package com.mattdepoorter.game_of_life;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TreeOfLifeApplication {
+public class GameOfLifeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TreeOfLifeApplication.class, args);
+		SpringApplication.run(GameOfLifeApplication.class, args);
 	}
 }
--- src/test/java/com/mattdepoorter/game_of_life/GameOfLifeApplicationTests.java
+++ src/test/java/com/mattdepoorter/game_of_life/GameOfLifeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mattdepoorter.tree_of_life;
+package com.mattdepoorter.game_of_life;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TreeOfLifeApplicationTests {
+public class GameOfLifeApplicationTests {
 
 	@Test
 	public void contextLoads() {
