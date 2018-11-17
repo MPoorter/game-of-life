3fd78a783bbf8e16812cc4978b58f225e1d03001
Matt <matt.depoorter@marshmallow.co>
2018-11-17 06:52:44 +0000

    Initial commit

---
+++ src/main/java/com/mattdepoorter/tree_of_life/TreeOfLifeApplication.java
@@ -0,0 +1,12 @@
+package com.mattdepoorter.tree_of_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeOfLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeOfLifeApplication.class, args);
+	}
+}
